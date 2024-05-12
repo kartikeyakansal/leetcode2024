c7d2a4f95763586ee67d0a466ab50ac0ab73b9ab
kartikeyakansal <59795753+kartikeyakansal@users.noreply.github.com>
2024-05-12 16:42:05 -0700

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        store = {}
+        for i in range(len(nums)):
+            if nums[i] in store.keys():
+                return [i, store[nums[i]]]
+            store[target - nums[i]] = i
+        return []
